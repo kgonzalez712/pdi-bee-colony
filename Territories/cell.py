b41a5b64bcc8e10c0e3f7dd34368f4a1c8e62aec
Kevin Gonzalez <kevin.gonzalezs0795@gmail.com>
2023-09-24 02:12:58 -0600

    refactor code

--- Territories/cell.py
+++ Territories/cell.py
@@ -1,14 +1,15 @@
 class Cell:
+    def __init__(self, value: int):
+        self.value = value
+        self.cellQuality = 0
+        self.visited = "F" if self.value == 0 else "-"
 
-  def __init__(self, value):
-    # If value = 0 is an empty space, if it is not 0 then is an obstacle
-    self.value = value
-    self.cellQuality = 0
-    if self.value == 0:
-      self.visited = "F"
-    else:
-      self.visited = "-"
-  
-  def reduceQuality(self):
-    self.cellQuality = self.cellQuality - 1
-    return self.cellQuality
+    def reduceQuality(self):
+        """
+        Reduces the quality of the cell by 1 and returns the updated quality.
+
+        Returns:
+             The updated quality of the cell.
+        """
+        self.cellQuality -= 1
+        return self.cellQuality
